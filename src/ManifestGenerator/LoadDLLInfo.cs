096f927a0735280c3de0d650c495936604762877
isc30 <ivansanzcarasa@gmail.com>
2020-04-15 06:59:08 +0200

    start working on the manifest generator

---
+++ src/ManifestGenerator/LoadDLLInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace BlazorLazyLoading
+{
+    public class LoadDLLInfo : Task
+    {
+        [Required]
+        public string EntryPoint { get; set; } = null!;
+
+        public override bool Execute()
+        {
+            Log.LogError("HELLO FROM TASK! {0}", EntryPoint);
+
+            return true;
+        }
+    }
+}
