fe4ef74463b51901cc286aa2371e80276ea74f51
isc30 <ivansanzcarasa@gmail.com>
2020-04-19 13:25:55 +0200

    [#16] Update to 3.2.0-preview4.20210.8

--- demo/ServerHost/Startup.cs
+++ demo/ServerHost/Startup.cs
@@ -1,8 +1,6 @@
-using System.Net.Mime;
 using BlazorLazyLoading.Server;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
