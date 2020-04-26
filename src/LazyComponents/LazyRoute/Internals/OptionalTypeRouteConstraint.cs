2cf02436c29cc4e76c18fdef1f632471d4165eeb
Ivan Sanz Carasa <ivansanzcarasa@gmail.com>
2020-04-26 12:07:30 +0200

    Functional Lazy Router! (#27)

---
+++ src/LazyComponents/LazyRoute/Internals/OptionalTypeRouteConstraint.cs
@@ -0,0 +1,37 @@
+﻿namespace BlazorLazyLoading.LazyRoute.Internals
+{
+    internal class OptionalTypeRouteConstraint<T> : RouteConstraint
+    {
+        public delegate bool TryParseDelegate(string str, out T result);
+
+        private readonly TryParseDelegate _parser;
+
+        public OptionalTypeRouteConstraint(TryParseDelegate parser)
+        {
+            _parser = parser;
+        }
+
+        public override bool Match(string pathSegment, out object convertedValue)
+        {
+            // Unset values are set to null in the Parameters object created in
+            // the RouteContext. To match this pattern, unset optional parmeters
+            // are converted to null.
+            if (string.IsNullOrEmpty(pathSegment))
+            {
+                convertedValue = null!;
+                return true;
+            }
+
+            if (_parser(pathSegment, out var result))
+            {
+                convertedValue = result!;
+                return true;
+            }
+            else
+            {
+                convertedValue = null!;
+                return false;
+            }
+        }
+    }
+}
---
+++ src/LazyComponents/LazyRoute/Internals/RouteTable.cs
@@ -0,0 +1,24 @@
+﻿namespace BlazorLazyLoading.LazyRoute.Internals
+{
+    internal class RouteTable
+    {
+        public RouteTable(RouteEntry[] routes)
+        {
+            Routes = routes;
+        }
+
+        public RouteEntry[] Routes { get; }
+
+        internal void Route(RouteContext routeContext)
+        {
+            for (var i = 0; i < Routes.Length; i++)
+            {
+                Routes[i].Match(routeContext);
+                if (routeContext.Handler != null)
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
