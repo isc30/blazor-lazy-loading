e2f446c4778a6ff5e3e7bb60cf33e038fbff3a9b
isc30 <ivansanzcarasa@gmail.com>
2020-05-01 12:33:54 +0200

    Update to 3.2.0-rc1.20223.4

--- src/AssemblyLoader/Helpers/HashCode.cs
+++ src/AssemblyLoader/Helpers/HashCode.cs
@@ -2,18 +2,18 @@
 
 namespace BlazorLazyLoading.Helpers
 {
-    public static class HashCode
-    {
-        public static int Combine(params object[] instances)
-        {
-            int hash = 17;
+    //public static class HashCode
+    //{
+    //    public static int Combine(params object[] instances)
+    //    {
+    //        int hash = 17;
 
-            foreach (var i in instances)
-            {
-                hash = unchecked((hash * 31) + (i?.GetHashCode() ?? 0));
-            }
+    //        foreach (var i in instances)
+    //        {
+    //            hash = unchecked((hash * 31) + (i?.GetHashCode() ?? 0));
+    //        }
 
-            return hash;
-        }
-    }
+    //        return hash;
+    //    }
+    //}
 }
