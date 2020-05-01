203717e51e31f94e85fd4b0382c4130b6d205a56
Ivan Sanz Carasa <ivansanzcarasa@gmail.com>
2020-05-01 12:37:05 +0200

    Update to 3.2.0-rc1.20223.4 (#38)

--- src/LazyComponents/LazyRoute/Internals/RouteTableFactory.cs
+++ src/LazyComponents/LazyRoute/Internals/RouteTableFactory.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
-using BlazorLazyLoading.Helpers;
+//using BlazorLazyLoading.Helpers;
 using Microsoft.AspNetCore.Components;
 
 namespace BlazorLazyLoading.LazyRoute.Internals
